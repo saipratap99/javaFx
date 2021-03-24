50277e975205ae418e9b1432a76c50384a274631
saipratap99 <122005067@sastra.ac.in>
2021-03-24 18:43:08 +0530

    Switching scenes in one stage

--- src/demo/first.java
+++ src/demo/first.java
@@ -8,14 +8,15 @@ import javafx.event.Event;
 import javafx.scene.Scene;  
 import javafx.scene.control.Button;  
 import javafx.stage.Stage;  
-import javafx.scene.layout.StackPane;  
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;  
 
 
 public class first extends Application{  
 	
 	// Button b1
 	Stage window;
-	Button b1;
+	Button b1,b2;
 	
     public static void main (String[] args)  
     {  
@@ -29,20 +30,27 @@ public class first extends Application{
     	window = arg0;
 		// sets the title for the stage (window)
 		window.setTitle("First Application in FX");
-		// new button component
-		b1 = new Button("Say Hello World!");
-		
-		// event listener for button
-		b1.setOnAction(e->{System.out.println("Hellow javafx!");});		
+		// button component
+		b1 = new Button("Switch to scene 2");
 		// layout
 		StackPane layout = new StackPane();
 		
 		// adding components in the layout
 		layout.getChildren().add(b1);
 		// Creating scene by passing layout, width and height of scene
-		Scene sc = new Scene(layout,600,300);
+		Scene sc1 = new Scene(layout,600,300);
+		
+		b2 = new Button("Go Back!");
+		VBox layout2= new VBox();
+		layout2.getChildren().add(b2);
+		Scene sc2 = new Scene(layout2,600,300);
+		
+		// adding event listeners
+		b1.setOnAction(e->{window.setScene(sc2);});		
+		b2.setOnAction(e->{window.setScene(sc1);});
+		
 		// adding scene to the stage
-		window.setScene(sc);
+		window.setScene(sc1);
 		// showing the application
 		window.show();
 		
