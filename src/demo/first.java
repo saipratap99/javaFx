858de93d0e20e82ea14e63853bb07b82eb336055
saipratap99 <122005067@sastra.ac.in>
2021-03-24 18:25:29 +0530

    First javafx app - adding stage,scene,button and event listner

---
+++ src/demo/first.java
@@ -0,0 +1,59 @@
+package demo;
+
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;  
+import javafx.scene.control.Button;  
+import javafx.stage.Stage;  
+import javafx.scene.layout.StackPane;  
+
+
+public class first extends Application{  
+	
+	// Button b1
+	Stage window;
+	Button b1;
+	
+    public static void main (String[] args)  
+    {  
+    	launch(args);
+    }
+
+    // javaFx application method
+
+    @Override
+	public void start(Stage arg0) throws Exception {
+    	window = arg0;
+		// sets the title for the stage (window)
+		window.setTitle("First Application in FX");
+		// new button component
+		b1 = new Button("Say Hello World!");
+		
+		// event listener for button
+		b1.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.print("Hello World!");
+			}
+			
+		});
+		
+		// layout
+		StackPane layout = new StackPane();
+		
+		// adding components in the layout
+		layout.getChildren().add(b1);
+		// Creating scene by passing layout, width and height of scene
+		Scene sc = new Scene(layout,600,300);
+		// adding scene to the stage
+		window.setScene(sc);
+		// showing the application
+		window.show();
+		
+	}
+
+}  
