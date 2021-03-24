3b478980bd8cd4b6733b0744c8addcccf2b1049f
saipratap99 <122005067@sastra.ac.in>
2021-03-24 19:02:00 +0530

    Adding alert box

--- src/demo/first.java
+++ src/demo/first.java
@@ -31,7 +31,7 @@ public class first extends Application{
 		// sets the title for the stage (window)
 		window.setTitle("First Application in FX");
 		// button component
-		b1 = new Button("Switch to scene 2");
+		b1 = new Button("Click to get Alert");
 		// layout
 		StackPane layout = new StackPane();
 		
@@ -40,14 +40,13 @@ public class first extends Application{
 		// Creating scene by passing layout, width and height of scene
 		Scene sc1 = new Scene(layout,600,300);
 		
-		b2 = new Button("Go Back!");
-		VBox layout2= new VBox();
-		layout2.getChildren().add(b2);
-		Scene sc2 = new Scene(layout2,600,300);
-		
+//		b2 = new Button("Go Back!");
+//		VBox layout2= new VBox();
+//		layout2.getChildren().add(b2);
+//		Scene sc2 = new Scene(layout2,600,300);
+
 		// adding event listeners
-		b1.setOnAction(e->{window.setScene(sc2);});		
-		b2.setOnAction(e->{window.setScene(sc1);});
+		b1.setOnAction(e->{AlertBox.display("Alert!", "Unauthorized access");});		
 		
 		// adding scene to the stage
 		window.setScene(sc1);
