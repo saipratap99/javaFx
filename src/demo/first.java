5ec1f32893208da819d06da251c17f7517456b99
saipratap99 <122005067@sastra.ac.in>
2021-03-26 15:56:07 +0530

    Grid Layout and textfield

--- src/demo/first.java
+++ src/demo/first.java
@@ -3,13 +3,13 @@ package demo;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.scene.Scene;  
-import javafx.scene.control.Button;  
-import javafx.stage.Stage;  
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;  
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
 
 
 public class first extends Application{  
@@ -17,6 +17,9 @@ public class first extends Application{
 	// Button b1
 	Stage window;
 	Button b1,b2;
+	Label user_label,pass_label;
+	TextField username,pass;
+	
 	boolean ans;
     public static void main (String[] args)  
     {  
@@ -33,23 +36,29 @@ public class first extends Application{
 		// button component
 		b1 = new Button("Click to get confirm box");
 		// layout
-		StackPane layout = new StackPane();
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10,10,10,10));
+		grid.setVgap(10);
+		grid.setHgap(10);
 		
-		// adding components in the layout
-		layout.getChildren().add(b1);
-		// Creating scene by passing layout, width and height of scene
-		Scene sc1 = new Scene(layout,600,300);
+		user_label = new Label("UserName");
+		GridPane.setConstraints(user_label, 0, 0);
 		
-//		b2 = new Button("Go Back!");
-//		VBox layout2= new VBox();
-//		layout2.getChildren().add(b2);
-//		Scene sc2 = new Scene(layout2,600,300);
-
-		// adding event listeners
-		b1.setOnAction(e->{System.out.println(ConfirmBox.display("Confirm!", "Are you sure?"));});		
+		username = new TextField();
+		GridPane.setConstraints(username, 1, 0);
 		
-		// adding scene to the stage
-		window.setScene(sc1);
+		pass_label = new Label("Password");
+		GridPane.setConstraints(pass_label, 0, 1);
+		
+		pass = new TextField();
+		GridPane.setConstraints(pass, 1, 1);
+		pass.setPromptText("Password");
+		
+		grid.getChildren().addAll(user_label,username,pass_label,pass);
+		
+		Scene sc= new Scene(grid,600,300);
+		
+		window.setScene(sc);
 		// showing the application
 		window.show();
 		
